d0df1fdf871e35f54323e404cea642d1b7724669
joyhong91 <remonkp@gmail.com>
2019-04-12 16:35:51 +0900

    init nuxt app

---
+++ components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Starting page</nuxt-link></li>
+        <li><nuxt-link to="/recipes">Recipes page</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<style scoped>
+  header{
+    width: 100%;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #ccc;
+  }
+  
+  ul {
+    list-style: none;
+    justify-content: center;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    align-items: center;
+  }
+  
+  li{
+    margin: 0 10px;
+  }
+  
+  a{
+    text-decoration: none;
+    color: white;
+  }
+</style>

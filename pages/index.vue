e7a153d8f6cd64e95a805b9462a9f64ef4c1fb58
joyhong91 <remonkp@gmail.com>
2019-04-15 11:06:49 +0900

    edit test

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <section class="container" id="alertMsg">
     <div>
+      <h1>Joy's portfolio</h1>
       <p class="title">
         준비중 / 공사중
       </p>
       <p class="subtitle">
         아직볼게 없어요....<br>
       </p>
-      <div>
+      <div class="main_img">
         <img src="https://cdn-images-1.medium.com/max/1200/1*oEuJgRJZrcjZBw-U95kl3Q.png" alt="nuxt+heroku"/>
       </div>
       <p class="desc">comming soon!</p>
@@ -29,9 +30,10 @@
 
 <style scoped>
   #alertMsg{text-align:center;margin-top:40px}
-  #alertMsg .title{font-size:50px;font-weight:bold}
+  #alertMsg .title{font-size:50px;font-weight:bold;margin-top:20px}
   #alertMsg .subtitle{font-size:25px;margin-top:20px}
   #alertMsg .desc{font-size:30px;font-weight:bold;text-transform:uppercase;margin-top:20px;margin-bottom:20px}
+  #alertMsg .main_img img{width:100%;height:auto}
 </style>
 <!--<script>-->
 <!--import Logo from '~/components/Logo.vue'-->
